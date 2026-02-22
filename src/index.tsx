16f0e32624929ef888b1d9164f584a694dceeaba
Andy Hawks <andy.hawks@civicactions.com>
2026-02-22 15:59:38 -0700

    update missing root in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App"; // Make sure this matches your main component filename
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = createRoot(rootElement!);
 
 root.render(
   <StrictMode>
