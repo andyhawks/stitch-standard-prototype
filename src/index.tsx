94b32a5b8d992c5c073387deed0cb224009c1b3f
Andy Hawks <andy.hawks@civicactions.com>
2026-02-22 15:43:26 -0700

    update App path in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App"; // Make sure this matches your main component filename
+import App from "./App.tsx"; // Make sure this matches your main component filename
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
